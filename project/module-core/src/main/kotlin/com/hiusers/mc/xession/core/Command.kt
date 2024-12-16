bbc5d1f2a5c1d46334d3b8b1c5ac87223a77b126
iiabc <hiusers@88.com>
2024-12-16 23:58:29 +0800

    补充命令

--- project/module-core/src/main/kotlin/com/hiusers/mc/xession/core/Command.kt
+++ project/module-core/src/main/kotlin/com/hiusers/mc/xession/core/Command.kt
@@ -2,9 +2,11 @@ package com.hiusers.mc.xession.core
 
 import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.command.CommandBody
+import taboolib.common.platform.command.CommandHeader
 import taboolib.common.platform.command.subCommand
 import taboolib.module.lang.sendLang
 
+@CommandHeader("xession", permission = "xession.command")
 object Command {
 
     @CommandBody(permission = "xession.command.admin.reload")
